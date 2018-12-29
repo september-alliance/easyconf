7ca97860f90a8f5f502256f5cd67fe612174f717
yexinzhou <yexinzhou@meipingmi.com.cn>
2018-12-29 18:10:23 +0800

    小优化

--- src/main/java/org/september/easyconf/controller/LoginController.java
+++ src/main/java/org/september/easyconf/controller/LoginController.java
@@ -1,7 +1,5 @@
 package org.september.easyconf.controller;
 
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +15,6 @@ public class LoginController {
 	@RequestMapping("/login")
 	public ModelAndView login(){
 		ModelAndView mv = new ModelAndView();
-		try {
-			logoName = new String(logoName.getBytes("ISO-8859-1") , "utf8");
-		} catch (UnsupportedEncodingException e) {
-		}
 		mv.addObject("logoName", logoName);
 		return mv;
 	}
